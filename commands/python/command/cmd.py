b0236f84e430330b624619adae2359893d776375
Josh Wilson <josh.wilson@fivestars.com>
2020-04-22 13:38:34 -0700

    Add support for commands-base:2.0.0
    The `commands-base:2.0.0` image moves the `command.py` script to a new location and name:
    `/usr/local/bin/aladdin_cmd`. It also assumes resposibility for ensuring it's executable so
    aladdin can just invoke the script or program. This is to allow developers to mount their host files
    into their command containers at `/code` without stomping on this necessary script.
    
    Since this is an interface breaking change, we are also adding a new `lamp.json` config value and
    structure to support it.
    
    ```
    {
        "name": "...",
        "build_docker": "...",
        "helm_chart": "...",
        "docker_images": [...],
        "commands": {
            "cmd": {
                "version": 2
            }
        }
    }
    ```
    
    Projects that wish to move to the new `commands-base:2.0.0` image should update their commands`
    Dockerfiles to derive from this image and then set the `commands.cmd.version` to `2`. This will
    instruct aladdin to invoke the command script from the new location. Additionally, if one wishes to
    use an alternative implementation of the command script provided by `commands-base`, one only needs
    to COPY their implementation to `/usr/local/bin/aladdin_cmd` and set it as executable.
    
    [$JIRA](https://fivestars.atlassian.net/browse/$JIRA)

--- commands/python/command/cmd.py
+++ commands/python/command/cmd.py
@@ -3,17 +3,27 @@ import logging
 import subprocess
 import sys
 
+from jmespath import search
+
 from arg_tools import add_namespace_argument
 from libs.k8s.kubernetes import Kubernetes
+from project.project_conf import ProjectConf
 
 
 def parse_args(sub_parser):
-    subparser = sub_parser.add_parser('cmd',
-                                      help='Run commands on specifc project via its commands pod')
-    subparser.add_argument('app_name', type=str, metavar='app_name',
-                           help='app to run commands against')
-    subparser.add_argument('command_args', type=str, metavar='comm_args',
-                           help='command and its args', nargs=argparse.REMAINDER)
+    subparser = sub_parser.add_parser(
+        "cmd", help="Run commands on specifc project via its commands pod"
+    )
+    subparser.add_argument(
+        "app_name", type=str, metavar="app_name", help="app to run commands against"
+    )
+    subparser.add_argument(
+        "command_args",
+        type=str,
+        metavar="comm_args",
+        help="command and its args",
+        nargs=argparse.REMAINDER,
+    )
     add_namespace_argument(subparser)
     subparser.set_defaults(func=cmd_args)
 
@@ -26,11 +36,34 @@ def cmd(app_name, command_args, namespace=None):
     k = Kubernetes(namespace=namespace)
     pod_name = k.get_pod_name("%s-commands" % app_name)
     logging.info("Command output below...")
-    try:
-        # See if the commands container uses python3
-        executable = k.kub_exec(pod_name, "%s-commands" % app_name, 'which', 'python3',
-                                return_output=True).decode(sys.stdout.encoding).strip()
-    except subprocess.CalledProcessError:
-        # If not, we assume it uses python
-        executable = 'python'
-    k.kub_exec(pod_name, "%s-commands" % app_name, executable, 'command.py', *command_args)
+
+    pc = ProjectConf()
+    cmd_config = search("cmd", pc.commands_config)
+
+    if search("version", cmd_config) == 2:
+        # In commands-base 2.0.0, we moved the commands.py script to /usr/local/bin/aladdin_cmd
+        # and made it executable. It's expected that the implementer will provide a she-bang line to
+        # indicate which interpreter to use (or none at all if they are using a compiled executable)
+        k.kub_exec(pod_name, "%s-commands" % app_name, "/usr/local/bin/aladdin_cmd", *command_args)
+    else:
+
+        logging.warning(
+            "commands-base:1.0.0 is deprecated."
+            " Update your commands images to commands-base:2.0.0"
+            " and set commands.cmd.version=2 in your lamp.json file."
+        )
+
+        try:
+            # See if the commands container uses python3
+            executable = (
+                k.kub_exec(
+                    pod_name, "%s-commands" % app_name, "which", "python3", return_output=True
+                )
+                .decode(sys.stdout.encoding)
+                .strip()
+            )
+        except subprocess.CalledProcessError:
+            # If not, we assume it uses python
+            executable = "python"
+
+        k.kub_exec(pod_name, "%s-commands" % app_name, executable, "command.py", *command_args)
