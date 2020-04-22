642031dfbac72174147ac8d79f645082f41205b6
Josh Wilson <josh.wilson@fivestars.com>
2020-04-22 16:27:08 -0700

    Test command container for new command script location

--- commands/python/command/cmd.py
+++ commands/python/command/cmd.py
@@ -3,11 +3,8 @@ import logging
 import subprocess
 import sys
 
-from jmespath import search
-
 from arg_tools import add_namespace_argument
 from libs.k8s.kubernetes import Kubernetes
-from project.project_conf import ProjectConf
 
 
 def parse_args(sub_parser):
@@ -37,20 +34,21 @@ def cmd(app_name, command_args, namespace=None):
     pod_name = k.get_pod_name("%s-commands" % app_name)
     logging.info("Command output below...")
 
-    pc = ProjectConf()
-    cmd_config = search("cmd", pc.commands_config)
-
-    if search("version", cmd_config) == 2:
-        # In commands-base 2.0.0, we moved the commands.py script to /usr/local/bin/aladdin_cmd
+    try:
+        # In commands-base:2.0.0, we moved the commands.py script to /usr/local/bin/aladdin_cmd
         # and made it executable. It's expected that the implementer will provide a she-bang line to
         # indicate which interpreter to use (or none at all if they are using a compiled executable)
-        k.kub_exec(pod_name, "%s-commands" % app_name, "/usr/local/bin/aladdin_cmd", *command_args)
-    else:
-
+        k.kub_exec(
+            pod_name,
+            "%s-commands" % app_name,
+            "/bin/bash",
+            "-c",
+            "test -x /usr/local/bin/aladdin_cmd",
+        )
+    except subprocess.CalledProcessError:
+        # commands-base:1.0.0 behavior
         logging.warning(
-            "commands-base:1.0.0 is deprecated."
-            " Update your commands images to commands-base:2.0.0"
-            " and set commands.cmd.version=2 in your lamp.json file."
+            "commands-base:1.0.0 is deprecated. Update your commands images to commands-base:2.0.0."
         )
 
         try:
@@ -67,3 +65,6 @@ def cmd(app_name, command_args, namespace=None):
             executable = "python"
 
         k.kub_exec(pod_name, "%s-commands" % app_name, executable, "command.py", *command_args)
+    else:
+        # commands-base:2.0.0 behavior
+        k.kub_exec(pod_name, "%s-commands" % app_name, "/usr/local/bin/aladdin_cmd", *command_args)
