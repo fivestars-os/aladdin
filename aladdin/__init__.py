9f1653134e5938fc37ebe94bf8a79721c043644d
Jose Rojas <39174181+jarojasm95@users.noreply.github.com>
2021-03-17 10:07:34 -0600

    fix for pre-py3.8

--- aladdin/__init__.py
+++ aladdin/__init__.py
@@ -1,4 +1,8 @@
-import importlib.metadata
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
 
-__version__ = importlib.metadata.version("aladdin")
+__version__ = metadata.version("aladdin")
 VERSION = tuple(__version__.split("."))
