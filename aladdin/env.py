b815c343a22864fdd5dfc3c8aac200ffa8f2a951
Jose Rojas <39174181+jarojasm95@users.noreply.github.com>
2021-11-08 12:21:24 -0700

    cleanup and add docstrings

--- aladdin/env.py
+++ aladdin/env.py
@@ -1,3 +1,6 @@
+"""
+Module to configure environment variables used by Aladdin
+"""
 import os
 import os
 import pathlib
@@ -7,7 +10,6 @@ from jmespath import search
 
 from aladdin import __version__, config
 from aladdin.lib import logging
-from aladdin.lib.utils import working_directory
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +21,11 @@ def configure_env():
         # default of True if not specified
         True if manage_software_dependencies is None else manage_software_dependencies
     )
-    if not os.getenv("ALADDIN_IMAGE") or not config.ALADDIN_DEV:
+
+    # Allow aladdin devs to use a custom aladdin image by setting ALADDIN_IMAGE
+    if not (os.getenv("ALADDIN_IMAGE") and config.ALADDIN_DEV):
+        # Set ALADDIN_IMAGE from the "aladdin.repo" config (or config.ALADDIN_DOCKER_REPO) and
+        # use the aladdin version as the image tag
         os.environ["ALADDIN_IMAGE"] = "{}:{}".format(
             search("aladdin.repo", config.load_config()) or config.ALADDIN_DOCKER_REPO,
             __version__
@@ -29,7 +35,8 @@ def configure_env():
 def set_config_path() -> bool:
     """
     Function to set the "ALADDIN_CONFIG_DIR" env var
-    Uses git to fetch the latest revision of the config repo
+    Uses git to fetch the latest revision of the config repo, the repo
+    is expected to have a branch or tag that matches the current aladdin version
 
     NOTE:
     If this function returns False it will short-circuit execution of
@@ -59,51 +66,62 @@ def set_config_path() -> bool:
         return False
 
     if not config_repo:
-        # try to auto-set config_repo
+        """
+        Some users might only have "config_dir" setup because
+        "config_repo" was introduced in v1.19.7.14
+
+        This code path makes the version upgrade smooth by automatically setting
+        "config_repo" from "config_dir"
+        """
         if not os.path.isdir(config_dir):
             logger.error(err_message)
             return False
-        with working_directory(config_dir):
-            try:
-                remote = subprocess.run(
-                    "git remote get-url origin".split(),
-                    check=True,
-                    capture_output=True,
-                    encoding="utf-8"
-                ).stdout.strip()
-            except subprocess.CalledProcessError:
-                logger.error(err_message)
-                return False
+        try:
+            remote = subprocess.run(
+                "git remote get-url origin".split(),
+                check=True,
+                capture_output=True,
+                encoding="utf-8",
+                cwd=config_dir,
+            ).stdout.strip()
+        except subprocess.CalledProcessError:
+            logger.error(err_message)
+            return False
         *_, git_account, repo = remote.split("/")
         config_repo = f"git@github.com:{git_account}/{repo}"
         user_config["config_repo"] = config_repo
         config.set_user_config_file(user_config)
 
-    remote_config_path = pathlib.Path.home() / ".aladdin/remote_config"
-    commands = [f"git clone -b {__version__} {config_repo} remote_config"]
+    # The config repo is expected to have a branch or tag matching the current aladdin version
+    git_commands = [f"git clone -b {__version__} {config_repo} remote_config"]
     cwd = pathlib.Path.home() / ".aladdin"
+    remote_config_path = cwd / "remote_config"
     if os.path.isdir(remote_config_path) and os.path.isdir(remote_config_path / ".git"):
+        """
+        The remote config has already been checked out,
+        we update git tags and checkout the latest revision
+        """
         cwd = remote_config_path
-        commands = [
+        git_commands = [
             "git fetch --tags --prune -f",
             f"git checkout {__version__}"
         ]
-    with working_directory(cwd):
-        for command in commands:
-            try:
-                subprocess.run(
-                    command.split(),
-                    check=True,
-                    encoding="utf-8",
-                    capture_output=True
-                )
-            except subprocess.CalledProcessError as e:
-                logger.error(
-                    "Failed to fetch aladdin config (%s) from remote: \n%s",
-                    config_repo,
-                    e.stderr.strip() or e.stdout.strip()
-                )
-                return False
+    for command in git_commands:
+        try:
+            subprocess.run(
+                command.split(),
+                check=True,
+                encoding="utf-8",
+                capture_output=True,
+                cwd=str(cwd),
+            )
+        except subprocess.CalledProcessError as e:
+            logger.error(
+                "Failed to fetch aladdin config (%s) from remote: \n%s",
+                config_repo,
+                e.stderr.strip() or e.stdout.strip()
+            )
+            return False
 
     os.environ["ALADDIN_CONFIG_DIR"] = str(remote_config_path)
 
