a51ed524ec4c4ce9271a8c7b53c515d3c1ac804f
Jose Rojas <39174181+jarojasm95@users.noreply.github.com>
2022-03-08 16:46:09 -0700

    skip

--- aladdin/lib/git.py
+++ aladdin/lib/git.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from aladdin.lib.utils import working_directory
 
@@ -20,20 +21,24 @@ class Git:
             subprocess.check_call(["git", "checkout", ref])
 
     @classmethod
+    def get_repo(cls):
+        origin = subprocess.check_output(["git", "remote", "get-url", "origin"], encoding="utf-8").strip()
+        return os.path.basename(origin)[:-4]
+
+    @classmethod
     def get_hash(cls):
         return cls._full_hash_to_short_hash(cls.get_full_hash())
 
     @classmethod
     def get_full_hash(cls):
-        cmd = ["git", "rev-parse", "HEAD"]
-        return subprocess.check_output(cmd).decode("utf-8").rstrip()
+        return subprocess.check_output(["git", "rev-parse", "HEAD"], encoding="utf-8").rstrip()
 
     @classmethod
     def _full_hash_to_short_hash(cls, full_hash):
         return full_hash[: cls.SHORT_HASH_SIZE]
 
     @classmethod
-    def extract_hash(cls, value, git_url):
+    def extract_hash(cls, value, git_url=None):
         """
         Get a hash out of whatever is the value given.
         value: can be a branch name, part of a hash
