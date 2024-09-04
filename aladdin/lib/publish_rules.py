d9ae18335d7095a288931f463243639938f92398
Jose Rojas <39174181+jarojasm95@users.noreply.github.com>
2024-09-04 12:56:19 -0600

    feat: Add support for value overlays (#175)

--- aladdin/lib/publish_rules.py
+++ aladdin/lib/publish_rules.py
@@ -9,5 +9,4 @@ class PublishRules:
     def __init__(self):
         publish_configs = load_publish_configs()
         boto = boto3.Session(profile_name=publish_configs["aws_profile"])
-        self.docker_registry = publish_configs["docker_ecr_repo"]
         self.ecr = boto.client("ecr")
